6a61160d11fc66e8232522d4b68bbb828f86e95f
root <root@ip-172-31-0-92.sa-east-1.compute.internal>
2017-08-30 11:27:38 +0000

    Added java sample project

---
+++ src/main/java/localdomain/localhost/tutorial/Main.java
@@ -0,0 +1,11 @@
+package localdomain.localhost.tutorial;
+public class Main {
+
+  public String concatenate(String one, String two){
+	return one + two;
+  }
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+    
+  }
+}
---
+++ src/test/java/localdomain/localhost/tutorial/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import localdomain.localhost.tutorial.*;
+public class MainTest {
+
+    @Test
+    public void testConcatenate() {
+        Main mytest = new Main();
+
+        String result = mytest.concatenate("one", "two");
+
+        assertEquals("one-two", result);
+
+    }
+}
